18b4615d1a0a8f27a75594778630067c6171f5ce
Dan Keenan <dk@dankeenan.org>
2018-09-02 12:43:02 -0400

    Migrate battle styles

---
+++ api/src/DataMigration/BattleStyle.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataMigration;
+
+use DragoonBoots\A2B\Annotations\DataMigration;
+use DragoonBoots\A2B\Annotations\IdField;
+use DragoonBoots\A2B\DataMigration\AbstractDataMigration;
+use DragoonBoots\A2B\DataMigration\DataMigrationInterface;
+use DragoonBoots\A2B\Drivers\DestinationDriverInterface;
+use DragoonBoots\A2B\Drivers\SourceDriverInterface;
+
+/**
+ * BattleStyle migration.
+ *
+ * @DataMigration(
+ *     name="BattleStyle",
+ *     source="csv:///%kernel.project_dir%/resources/data/battle_style.csv",
+ *     sourceIds={@IdField(name="identifier", type="string")},
+ *     destination="doctrine:///App/Entity/BattleStyle",
+ *     destinationIds={@IdField(name="id")}
+ * )
+ */
+class BattleStyle extends AbstractDoctrineDataMigration implements DataMigrationInterface
+{
+
+    /**
+     * {@inheritdoc}
+     * @param \App\Entity\BattleStyle $destinationData
+     */
+    public function transform($sourceData, $destinationData)
+    {
+        unset($sourceData['identifier']);
+
+        $properties = array_keys($sourceData);
+        $destinationData = $this->mergeProperties($properties, $sourceData, $destinationData);
+
+        return $destinationData;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function defaultResult()
+    {
+        return new \App\Entity\BattleStyle();
+    }
+}
---
+++ api/src/DataMigration/Veekun/BattleStyle.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\DataMigration\Veekun;
+
+use DragoonBoots\A2B\Annotations\DataMigration;
+use DragoonBoots\A2B\Annotations\IdField;
+use DragoonBoots\A2B\DataMigration\AbstractDataMigration;
+use DragoonBoots\A2B\DataMigration\DataMigrationInterface;
+use DragoonBoots\A2B\Drivers\DestinationDriverInterface;
+use DragoonBoots\A2B\Drivers\Source\DbalSourceDriver;
+use DragoonBoots\A2B\Drivers\SourceDriverInterface;
+
+/**
+ * BattleStyle migration.
+ *
+ * @DataMigration(
+ *     name="BattleStyle",
+ *     group="Veekun",
+ *     source="veekun",
+ *     sourceIds={@IdField(name="id")},
+ *     destination="csv:///%kernel.project_dir%/resources/data/battle_style.csv",
+ *     destinationIds={@IdField(name="identifier", type="string")}
+ * )
+ */
+class BattleStyle extends AbstractDataMigration implements DataMigrationInterface
+{
+
+    /**
+     * {@inheritdoc}
+     * @param DbalSourceDriver $sourceDriver
+     */
+    public function configureSource(SourceDriverInterface $sourceDriver)
+    {
+        $statement = $sourceDriver->getConnection()->prepare(
+            <<<SQL
+SELECT "move_battle_styles"."id",
+       "move_battle_styles"."identifier",
+       "move_battle_style_prose"."name"
+FROM "move_battle_styles"
+     JOIN "move_battle_style_prose"
+         ON "move_battle_styles"."id" = "move_battle_style_prose"."move_battle_style_id"
+WHERE "move_battle_style_prose"."local_language_id" = 9;
+SQL
+        );
+        $sourceDriver->setStatement($statement);
+
+        $countStatement = $sourceDriver->getConnection()->prepare(
+            <<<SQL
+SELECT count(*)
+FROM "move_battle_styles";
+SQL
+        );
+        $sourceDriver->setCountStatement($countStatement);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($sourceData, $destinationData)
+    {
+        unset($sourceData['id']);
+        $destinationData = array_merge($sourceData, $destinationData);
+
+        return $destinationData;
+    }
+}
