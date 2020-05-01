9bd9330a97d4551ae733dc366fe1dc6b618414e9
Dan Keenan <dk@dankeenan.org>
2020-05-01 15:05:54 -0400

    Rip Colo/XD Moves

--- scripts/gen3_gc/gen3_gc.py
+++ scripts/gen3_gc/gen3_gc.py
@@ -2,28 +2,34 @@ import argparse
 from pathlib import Path
 
 from gen3_gc import strings
+from inc import group_by_version_group
 from .enums import Version
-from .moves import get_moves
+from .moves import get_moves, write_moves
 
 if __name__ == '__main__':
     strings.register_codec()
 
+
+    def to_path(path: str):
+        return Path(path)
+
+
     # Get config
     argparser = argparse.ArgumentParser(description='Load Colosseum/XD data')
-    argparser.add_argument('--colosseum_dir', type=lambda path: Path(path), required=True, help='Colosseum directory')
-    argparser.add_argument('--xd_dir', type=lambda path: Path(path), required=True, help='XD directory')
+    argparser.add_argument('--colosseum_dir', type=to_path, required=True, help='Colosseum directory')
+    argparser.add_argument('--xd_dir', type=to_path, required=True, help='XD directory')
     argparser.add_argument('--version', action='append', type=lambda version: Version(version),
                            choices=list(Version), help='Version slug to dump')
 
-    argparser.add_argument('--out-pokemon', type=str, required=True, help='Pokemon YAML file dir')
-    argparser.add_argument('--out-pokemon_moves', type=str, required=True, help='Pokemon Move CSV file')
-    argparser.add_argument('--out-moves', type=str, required=True, help='Move YAML file dir')
-    argparser.add_argument('--out-contest_effects', type=str, required=True, help='Contest Effect YAML file dir')
-    argparser.add_argument('--out-items', type=str, required=True, help='Item YAML file dir')
-    argparser.add_argument('--out-shop_items', type=str, required=True, help='Shop Data CSV file')
-    argparser.add_argument('--out-locations', type=str, required=True, help='Location YAML file dir')
-    argparser.add_argument('--out-encounters', type=str, required=True, help='Encounter CSV file')
-    argparser.add_argument('--out-abilities', type=str, required=True, help='Ability YAML file dir')
+    argparser.add_argument('--out-pokemon', type=to_path, required=True, help='Pokemon YAML file dir')
+    argparser.add_argument('--out-pokemon_moves', type=to_path, required=True, help='Pokemon Move CSV file')
+    argparser.add_argument('--out-moves', type=to_path, required=True, help='Move YAML file dir')
+    argparser.add_argument('--out-contest_effects', type=to_path, required=True, help='Contest Effect YAML file dir')
+    argparser.add_argument('--out-items', type=to_path, required=True, help='Item YAML file dir')
+    argparser.add_argument('--out-shop_items', type=to_path, required=True, help='Shop Data CSV file')
+    argparser.add_argument('--out-locations', type=to_path, required=True, help='Location YAML file dir')
+    argparser.add_argument('--out-encounters', type=to_path, required=True, help='Encounter CSV file')
+    argparser.add_argument('--out-abilities', type=to_path, required=True, help='Ability YAML file dir')
 
     argparser.add_argument('--write-pokemon', action='store_true', help='Write Pokemon data')
     argparser.add_argument('--write-pokemon_moves', action='store_true', help='Write Pokemon move data')
@@ -56,7 +62,8 @@ if __name__ == '__main__':
         dump_path = paths[version]
         print('Using version {version}'.format(version=version.value))
 
-        out_moves = get_moves(dump_path, version)
+        vg_moves, vg_move_slugs = get_moves(dump_path, version, args.out_moves)
+        out_moves = group_by_version_group(version.value, vg_moves, out_moves)
 
         dumped_versions.add(version.value)
 
@@ -70,4 +77,6 @@ if __name__ == '__main__':
             roms=', '.join(missing_versions)))
         exit(1)
     else:
+        if args.write_moves:
+            write_moves(out_moves, args.out_moves)
         exit(0)
--- scripts/gen3_gc/moves.py
+++ scripts/gen3_gc/moves.py
@@ -3,22 +3,27 @@ from io import BufferedReader
 from pathlib import Path
 import struct
 
+import progressbar
 from slugify import slugify
 
-from gen3_gc.enums import type_map, Version
+from gen3_gc.enums import move_name_changes, type_map, Version
 from gen3_gc.strings import ColoStrings, get_string, XdStrings
+from inc.yaml import yaml
+
+move_slugs = {}
+out = {}
 
 
-def get_moves(game_path: Path, version: Version):
-    move_slugs = {}
-    out = {}
-
-    common_rel_path = (game_path / 'common.fsys' / 'common_rel.fdat')
+def get_moves(game_path: Path, version: Version, moves_out_path: Path):
+    common_rel_path = (game_path.joinpath(Path('common.fsys/common_rel.fdat')))
     assert common_rel_path.is_file()
 
     common_rel: BufferedReader
     with common_rel_path.open('rb') as common_rel:
-        out, move_slugs = _get_stats(game_path, common_rel, version)
+        _get_stats(game_path, common_rel, version)
+    _pullup_data(version, moves_out_path)
+
+    return out, move_slugs
 
 
 def _get_stats(game_path: Path, common_rel: BufferedReader, version: Version):
@@ -53,12 +58,10 @@ def _get_stats(game_path: Path, common_rel: BufferedReader, version: Version):
         6: 'all-other-pokemon',
         7: 'opponents-field',
     }
-
-    def is_shadow(move_id: int):
-        return move_id >= 355
-
-    out = {}
-    move_slugs = {}
+    damage_class_map = {
+        1: 'physical',
+        2: 'special',
+    }
 
     @dataclass()
     class MoveBaseStats:
@@ -67,7 +70,7 @@ def _get_stats(game_path: Path, common_rel: BufferedReader, version: Version):
                 data = struct.unpack('>bBBBBB??????4x?x?B3xB3xB6xH10xH2xH4x', data)
                 self.priority = data[0]
                 self.pp = data[1]
-                self.type = type_map[data[2]]
+                self.typeId = data[2]
                 self.targetId = data[3]
                 self.accuracy = data[4]
                 self.effectChance = data[5]
@@ -79,7 +82,7 @@ def _get_stats(game_path: Path, common_rel: BufferedReader, version: Version):
                 self.kingsRock = data[11]
                 self.sound = data[12]
                 self.isHmMove = data[13]
-                self.physicalSpecial = None
+                self.damageClassId = None
                 self.recoil = data[14]
                 self.power = data[15]
                 self.effectId = data[16] + 1
@@ -87,10 +90,10 @@ def _get_stats(game_path: Path, common_rel: BufferedReader, version: Version):
                 self.descriptionId = data[18]
                 self.animationId = data[19]
             else:
-                data = struct.unpack('>bBBBBB??????4x?x??B4xB3xB4xH10xH2xH4x', data)
+                data = struct.unpack('>bBBBBB??????4x?x?BB4xB3xB4xH10xH2xH4x', data)
                 self.priority = data[0]
                 self.pp = data[1]
-                self.type = type_map[data[2]]
+                self.typeId = data[2]
                 self.targetId = data[3]
                 self.accuracy = data[4]
                 self.effectChance = data[5]
@@ -102,13 +105,19 @@ def _get_stats(game_path: Path, common_rel: BufferedReader, version: Version):
                 self.kingsRock = data[11]
                 self.sound = data[12]
                 self.isHmMove = data[13]
-                self.physicalSpecial = data[14]
+                self.damageClassId = data[14]
                 self.recoil = data[15]
                 self.power = data[16]
                 self.effectId = data[17] + 1
                 self.nameId = data[18]
                 self.descriptionId = data[19]
                 self.animationId = data[20]
+
+    def is_shadow(move_id: int):
+        # The shadow moves are defined internally as normal type, so this extra check is needed.
+        return move_id >= 355
+
+    print('Dumping moves')
 
     for move_id in range(1, num_moves + 1):
         common_rel.seek(stats_offset + ((move_id - 1) * stats_length))
@@ -118,19 +127,33 @@ def _get_stats(game_path: Path, common_rel: BufferedReader, version: Version):
             continue
         name = get_string(game_path, name_table, move_stats.nameId)
         slug = slugify(name)
+        if slug == 'solid-plant' and version == Version.XD:
+            # Dummy move
+            continue
         move_slugs[move_id] = slug
         flavor = get_string(game_path, flavor_table, move_stats.descriptionId)
         out[slug] = {
             'name': name,
             'power': move_stats.power,
-            'type': move_stats.type,
+            'type': type_map[move_stats.typeId],
             'accuracy': move_stats.accuracy,
             'pp': move_stats.pp,
             'effect': move_stats.effectId,
             'target': target_map[move_stats.targetId],
             'priority': move_stats.priority,
+            'damage_class': None,
             'flavor_text': flavor,
         }
+        if version == Version.COLOSSEUM:
+            del out[slug]['damage_class']
+        else:
+            # XD is the first game with move damage classes!  Kind of.  Only for shadow moves.
+            if move_stats.damageClassId > 0:
+                out[slug]['damage_class'] = damage_class_map[move_stats.damageClassId]
+            elif move_stats.typeId < 0x09:
+                out[slug]['damage_class'] = 'physical'
+            else:
+                out[slug]['damage_class'] = 'special'
 
         if is_shadow(move_id):
             out[slug]['type'] = 'shadow'
@@ -156,4 +179,80 @@ def _get_stats(game_path: Path, common_rel: BufferedReader, version: Version):
         if len(flags) > 0:
             out[slug]['flags'] = flags
 
-    return out, move_slugs
+
+def _pullup_data(version: Version, moves_out_path: Path):
+    pullup_keys = [
+        'crit_rate_bonus',
+        'drain',
+        'flinch_chance',
+        'ailment',
+        'ailment_chance',
+        'recoil',
+        'healing',
+        'categories',
+        'hits',
+        'turns',
+        'stat_changes',
+        'stat_change_chance',
+    ]
+    print('Using existing data')
+    for move_slug in progressbar.progressbar(move_slugs.values()):
+        if move_slug in move_name_changes:
+            file_slug = move_name_changes[move_slug]
+        elif move_slug == 'solid-plant':
+            # Not a name change per se, but this was mis-translated for Colosseum.
+            file_slug = 'frenzy-plant'
+        else:
+            file_slug = move_slug
+        yaml_path = moves_out_path.joinpath('{move}.yaml'.format(move=file_slug))
+
+        with yaml_path.open('r') as move_yaml:
+            old_move_data = yaml.load(move_yaml.read())
+            if version.value not in old_move_data:
+                # If the name has changed, try the original name, as it may have been moved already.
+                if move_slug in move_name_changes:
+                    yaml_path = moves_out_path.joinpath('{move}.yaml'.format(move=move_slug))
+                    with yaml_path.open('r') as move_yaml:
+                        old_move_data = yaml.load(move_yaml.read())
+                else:
+                    raise Exception(
+                        'Move {move} has no data for version group {version_group}.'.format(
+                            move=move_slug,
+                            version_group=version.value))
+        _pullup_keys = pullup_keys.copy()
+        if out[move_slug]['type'] == 'shadow' and version == Version.XD:
+            # The shadow move effects have tons of special cases that have been covered already.
+            _pullup_keys.append('effect')
+        for key in _pullup_keys:
+            if key in old_move_data[version.value]:
+                out[move_slug][key] = old_move_data[version.value][key]
+
+
+def write_moves(out_data: dict, moves_out_path: Path):
+    print('Writing Moves')
+    used_version_groups = set()
+    for move_slug, move_data in progressbar.progressbar(out_data.items()):
+        yaml_path = moves_out_path.joinpath('{slug}.yaml'.format(slug=move_slug))
+        try:
+            with yaml_path.open('r') as move_yaml:
+                data = yaml.load(move_yaml.read())
+        except IOError:
+            data = {}
+        data.update(move_data)
+        used_version_groups.update(move_data.keys())
+        with yaml_path.open('w') as move_yaml:
+            yaml.dump(data, move_yaml)
+
+    # Remove this version group's data from the new name's file
+    for old_name, new_name in move_name_changes.items():
+        yaml_path = moves_out_path.joinpath('{slug}.yaml'.format(slug=new_name))
+        with yaml_path.open('r') as move_yaml:
+            data = yaml.load(move_yaml.read())
+        changed = False
+        for check_version_group in used_version_groups:
+            if check_version_group in data:
+                del data[check_version_group]
+                changed = True
+        if changed:
+            with yaml_path.open('w') as move_yaml:
+                yaml.dump(data, move_yaml)
